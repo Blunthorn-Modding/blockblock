7cda4bcb44908e5c5237d2838296057bc5ecc1bd
WouterB15 <w.barf@live.nl>
2024-02-28 23:01:40 +0100

    Fixed totem and brush not working

--- src/main/java/net/wouterb/blockblock/BlockBlock.java
+++ src/main/java/net/wouterb/blockblock/BlockBlock.java
@@ -1,14 +1,20 @@
 package net.wouterb.blockblock;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
 import net.wouterb.blockblock.config.LockedDefaultValues;
 import net.wouterb.blockblock.config.ModConfigManager;
 import net.wouterb.blockblock.util.IEntityDataSaver;
--- src/main/java/net/wouterb/blockblock/mixin/entity/LivingEntityMixin.java
+++ src/main/java/net/wouterb/blockblock/mixin/entity/LivingEntityMixin.java
@@ -5,13 +5,18 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.wouterb.blockblock.util.IPlayerPermissionHelper;
+import net.wouterb.blockblock.util.ItemUsageMixinHelper;
 import net.wouterb.blockblock.util.ModLockManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
@@ -28,6 +33,15 @@ public class LivingEntityMixin {
             if (((IPlayerPermissionHelper) player).isEntityLocked(entityId, ModLockManager.LockType.ENTITY_DROP)){
                 ModLockManager.sendLockedFeedbackToPlayer(player, ModLockManager.LockType.ITEM_USAGE, localizedName);
                 ci.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "tryUseTotem", at=@At("HEAD"), cancellable = true)
+    private void tryUseTotem(DamageSource source, CallbackInfoReturnable<Boolean> ci) {
+        if (((Object)this) instanceof PlayerEntity player) {
+            if (ItemUsageMixinHelper.isItemLocked(player, Items.TOTEM_OF_UNDYING.getDefaultStack())) {
+                ci.setReturnValue(false);
             }
         }
     }
